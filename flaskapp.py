c290f6807a315d629ae5e38205f558f605a3ee57
Manas <manas23601@gmail.com>
2021-09-26 14:19:46 +0530

    implemented search feature

--- flaskapp.py
+++ flaskapp.py
@@ -1,20 +1,30 @@
-from flask import Flask, render_template, url_for
-from home import get_stats
+from flask import Flask, render_template, url_for, request
+from werkzeug.utils import redirect
+# from requests.api import request
+from home import get_stats, get_repo
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    data = []
+    if request.method == 'GET':
+        return render_template('home.html', data = data)
+    else:
+        name = request.form.get("repo")
+        data = get_repo(name)
+        return redirect(f"/chart/{data[0][1]}")
+        # return render_template('home.html', data = data)
+
 
 @app.route("/new_page")
 def new_page():
     return "<h1>New Page</h1>"
 
-@app.route("/chart")
-def google_pie_chart():
+@app.route("/chart/<id>")
+def google_pie_chart(id):
 	data = {'Problem' : 'Frequency'}
-	data = get_stats(data)
+	data = get_stats(data, id)
 	return render_template('pie-chart.html', data=data)
 
 if __name__ == '__main__':
--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 from flask.wrappers import Response
 from requests import get
 
+
 def get_data():
     response = get('https://dev-api.metabob.com/repository/72/analysis?include=problems')
     if response.status_code == 200:
@@ -12,8 +13,8 @@ def get_data():
         print('failed')
     return
 
-def get_stats(data):
-    response = get('https://dev-api.metabob.com/repository/72/analysis?include=stats')
+def get_stats(data, id):
+    response = get(f'https://dev-api.metabob.com/repository/{id}/analysis?include=stats')
     if response.status_code == 200:
         response = response.json()  
         response = response['stats']
@@ -23,3 +24,13 @@ def get_stats(data):
         return data
     else:
         return {}
+
+def get_repo(name):
+    i = 0
+    data = []
+    response = get(f'https://dev-api.metabob.com/repositories/?current_page=0&page_size=100')
+    response = response.json()
+    for rep in response:
+        if rep["name"] == name:
+            data.append([rep["name"], rep["id"]])
+    return data
